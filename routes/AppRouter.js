f216f12fb0714c0236ee254e4e35e7f260bd3c82
Paige Layman <paigelayman96@gmail.com>
2022-12-09 11:47:46 -0500

    created AppRouter content

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,3 +1,15 @@
 const Router = require('express').Router()
+const UserRouter = require('./UserRouter')
+const ReviewRouter = require('./ReviewRouter')
+const CityRouter = require('./CityRouter')
+const HospitalRouter = require('./HospitalRouter')
+const ExperienceRouter = require('./ExperienceRouter')
+
+Router.use('/users', UserRouter)
+Router.use('/reviews', ReviewRouter)
+Router.use('/cities', CityRouter)
+Router.use('/hospitals', HospitalRouter)
+Router.use('/experiences', ExperienceRouter)
+// Router.use('/auth', AuthRouter)
 
 module.exports = Router
