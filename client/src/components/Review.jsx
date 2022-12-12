da1115b2b2dfccca451e131081adaa65b44cdf1a
Paige Layman <paigelayman96@gmail.com>
2022-12-12 15:43:38 -0500

    reviews rendering

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -5,11 +5,28 @@ import axios from 'axios'
 
 
 const Review = () => {
-  
+  const [reviews, showReviews] = useState([])
+  useEffect(() => {
+    const apiCall = async () => {
+    let response = await axios.get(`http://localhost:3001/api/reviews`)
+    showReviews(response.data)
+    }
+    apiCall()
+  }, [])  
   
 return (
-  <div className="Home">
-    <h1>Review</h1>  
+  <div className="reviews">
+  <h1>Reviews</h1>
+  {reviews ? reviews.map((review) => (
+  <div className="reviews" key={review._id}>
+    <h2>Posted by: {review.name}</h2>
+      <h3>Unit: {review.unit}</h3>
+      <h3>Comments: {review.review}</h3>
+      <h3>Typical Patient Load: {review.patientLoad}</h3>
+      <h3>Overall Rating:{review.rating}/5</h3>
+      
+  </div>
+  )): "" }
   </div>
   )
 }
