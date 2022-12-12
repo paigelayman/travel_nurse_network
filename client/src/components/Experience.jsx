dd9d1b93658c8b55e41c8a02da42750030ece43d
Paige Layman <paigelayman96@gmail.com>
2022-12-12 15:50:16 -0500

    experiences rendering

--- client/src/components/Experience.jsx
+++ client/src/components/Experience.jsx
@@ -5,13 +5,27 @@ import axios from 'axios'
 
 
 const Experience = () => {
-  
+  const [experiences, showExperiences] = useState([])
+  useEffect(() => {
+    const apiCall = async () => {
+    let response = await axios.get(`http://localhost:3001/api/experiences`)
+    showExperiences(response.data)
+    }
+    apiCall()
+  }, [])  
   
 return (
-  <div className="Home">
-    <h1>Experience</h1>  
+  <div className="experiences">
+  <h1>Things To Do</h1>
+  {experiences ? experiences.map((experience) => (
+  <div className="experiences" key={experience._id}>
+    <img src={experience.photoLink} id="experience-image" alt="experience" />
+    <h3>Description: {experience.comment}</h3>
+    <h3>Price: ${experience.price}</h3>  
   </div>
-  )
+  )): "" }
+  </div>
+)
 }
 
 
