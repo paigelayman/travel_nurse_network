87a9b739d3040414a567390dc22abdb02c521520
Paige Layman <paigelayman96@gmail.com>
2022-12-14 16:33:28 -0500

    heroku deployment

--- config/config.js
+++ config/config.js
@@ -9,6 +9,7 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
+    use_env_variables: 'DATABASE_URL',
     database: 'travel_rn_production',
     dialect: 'postgres',
     dialectOptions: {
