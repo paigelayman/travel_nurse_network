ff29fe1fb7bd4a89dfdedce0b778d6c1d13b39e3
Paige Layman <paigelayman96@gmail.com>
2022-12-14 14:04:16 -0500

    more css

--- client/src/components/ReviewPage.jsx
+++ client/src/components/ReviewPage.jsx
@@ -7,6 +7,12 @@ const ReviewPage = () => {
   let { id } = useParams()
   const [reviews, setReviews] = useState([])
   const [formState, setFormState] = useState({ author: '', unit: '', patientLoad: '', review: '', rating: ''})
+  const [isShown, setIsShown] = useState(false)
+
+  const handleClick = (event) => {
+    event.preventDefault()
+    setIsShown(true)
+  }
 
   const handleChange = (event) => {
     setFormState({ ...formState, [event.target.id]: event.target.value })
@@ -21,7 +27,6 @@ const ReviewPage = () => {
   }, [])
 
   const handleSubmit = async (event) => {
-    // event.preventDefault()
     let addedReview = await axios
       .post(`http://localhost:3001/api/reviews/${id}`, formState)
       .then((response) => {
@@ -36,7 +41,6 @@ const ReviewPage = () => {
     }
 
     const handleUpdate = async (event) => {
-      // event.preventDefault()
       let fixedReview = await axios.put(`http://localhost:3001/api/reviews/${event.target.id}`, formState)
       console.log(event)
       setReviews([reviews, fixedReview.data])
@@ -44,14 +48,13 @@ const ReviewPage = () => {
     }
 
     const handleDelete = async (event) => {
-      // event.preventDefault()
       let deletedReview = await axios.delete(`http://localhost:3001/api/reviews/${event.target.id}`, formState)
       setReviews([reviews, deletedReview.data])
     }
 
 
   return (
-    <div className="ReviewPage" id="reviewCard">
+    <div className="ReviewPage">
       {reviews ? reviews.map((review) => (
       <div className="reviews" key={review.id}>
       <h3>Review by: {review.author}</h3>
@@ -60,16 +63,24 @@ const ReviewPage = () => {
         <h4>Comments: {review.review}</h4>
         <h4>Typical Patient Load: {review.patientLoad}</h4>
         <h4>Overall Rating:{review.rating}/5</h4>
-        <form className='update'>
-        <h3>Update Review: </h3>
-        <label htmlFor="review">Review:</label>
-        <input id="review" value={formState.description} onChange={handleChange} />
-        <label htmlFor="rating">Rating (out of 5): </label>
-        <input id="rating" value={formState.name} onChange={handleChange} />
-        {/* <label htmlFor="id">ID: </label>  */}
-      {/* <input id="id" value={formState.id} onChange={handleChange} /> */}
-        <button id={review.id} className='update' onClick={handleUpdate}>Update</button>
-        <button className='delete' id={review.id} onClick={handleDelete}>Delete</button>
+      <form>
+        <button onClick={handleClick}>Update or Delete Review</button>
+        {isShown && (
+        <div className='form-div'>
+          <h3>Update Review: </h3>
+          <label htmlFor="author">Your Name: </label>
+          <input id="author" value={formState.name} onChange={handleChange} />
+          <label htmlFor="unit">Unit:</label>
+          <input id="unit" value={formState.description} onChange={handleChange} />
+          <label htmlFor="patientLoad">Patient Load: </label>
+          <input id="patientLoad" value={formState.name} onChange={handleChange} />
+          <label htmlFor="review">Review:</label>
+          <input id="review" value={formState.description} onChange={handleChange} />
+          <label htmlFor="rating">Rating (out of 5): </label>
+          <input id="rating" value={formState.name} onChange={handleChange} />
+          <button id={review.id} className='update' onClick={handleUpdate}>Submit Update</button>
+          <button className='delete' id={review.id} onClick={handleDelete}>Delete Review</button>
+        </div> )}
       </form> 
     </div>
     )): "" }
