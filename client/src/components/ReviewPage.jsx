1edc84dadc7cd6cbbbe632281dada21999ba09d9
Paige Layman <paigelayman96@gmail.com>
2022-12-14 11:49:12 -0500

    delete review working on backend

--- client/src/components/ReviewPage.jsx
+++ client/src/components/ReviewPage.jsx
@@ -35,7 +35,19 @@ const ReviewPage = () => {
     setFormState({ author: '', unit: '', patientLoad: '', review: '', rating: ''})
     }
 
-    // const handleUpdate 
+    const handleUpdate = async (event) => {
+      event.preventDefault()
+      let fixedReview = await axios.put(`http://localhost:3001/api/reviews/${id}`, formState)
+      setReviews([reviews, fixedReview.data])
+      setFormState({ author: '', unit: '', patientLoad: '', review: '', rating: ''})
+    }
+
+    const handleDelete = async (event) => {
+      event.preventDefault()
+      let deletedReview = await axios.delete(`http://localhost:3001/api/reviews/${id}`, formState)
+      setReviews([reviews, deletedReview.data])
+      setFormState({ author: '', unit: '', patientLoad: '', review: '', rating: ''})
+    }
 
 
   return (
@@ -43,15 +55,30 @@ const ReviewPage = () => {
       {reviews ? reviews.map((review) => (
       <div className="reviews" key={review._id}>
       <h3>Review by: {review.author}</h3>
-        <h3>Unit: {review.unit}</h3>
-        <h3>Comments: {review.review}</h3>
-        <h3>Typical Patient Load: {review.patientLoad}</h3>
-        <h3>Overall Rating:{review.rating}/5</h3>
+        <h4>Unit: {review.unit}</h4>
+        <h4>Comments: {review.review}</h4>
+        <h4>Typical Patient Load: {review.patientLoad}</h4>
+        <h4>Overall Rating:{review.rating}/5</h4>
+        <button onClick={handleDelete}>Delete Review</button>
+      <form onSubmit={handleUpdate}>
+      <h3>Update Review: </h3>
+      <label htmlFor="author">Your Name: </label>
+      <input id="author" value={formState.name} onChange={handleChange} />
+      <label htmlFor="unit">Unit:</label>
+      <input id="unit" value={formState.description} onChange={handleChange} />
+      <label htmlFor="patientLoad">Patient Load: </label>
+      <input id="patientLoad" value={formState.name} onChange={handleChange} />
+      <label htmlFor="review">Review:</label>
+      <input id="review" value={formState.description} onChange={handleChange} />
+      <label htmlFor="rating">Rating (out of 5): </label>
+      <input id="rating" value={formState.name} onChange={handleChange} />
+      <button className='submit' type="submit">Update</button>
+      </form>
     </div>
     )): "" }
       <div>
     <form onSubmit={handleSubmit}>
-      <h3>Add Review: </h3>
+      <h3>Add Your Own Review: </h3>
       <label htmlFor="author">Your Name: </label>
       <input id="author" value={formState.name} onChange={handleChange} />
       <label htmlFor="unit">Unit:</label>
