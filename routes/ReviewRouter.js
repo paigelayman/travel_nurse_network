3c1d730f36e5ce39771ebe30b5864d6ca34c6971
Paige Layman <paigelayman96@gmail.com>
2022-12-14 11:09:53 -0500

    post review working on backend

--- routes/ReviewRouter.js
+++ routes/ReviewRouter.js
@@ -3,8 +3,8 @@ const controller = require('../controllers/ReviewController')
 
 Router.get('/', controller.getReviews)
 Router.get('/:hospital_id', controller.getHospitalReviews)
-Router.post('/', controller.addReview)
-Router.put('/:review_id', controller.updateReview)
-Router.delete('/:review_id', controller.deleteReview)
+Router.post('/:hospital_id', controller.addReview)
+Router.put('/:hospital_id', controller.updateReview)
+Router.delete('/:hospital_id', controller.deleteReview)
 
 module.exports = Router
