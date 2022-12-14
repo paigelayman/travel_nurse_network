e790e19b060cad16539d966c021d852fca0cd0b1
Paige Layman <paigelayman96@gmail.com>
2022-12-14 13:35:58 -0500

    lots of debugging

--- client/src/components/ReviewPage.jsx
+++ client/src/components/ReviewPage.jsx
@@ -21,7 +21,7 @@ const ReviewPage = () => {
   }, [])
 
   const handleSubmit = async (event) => {
-    event.preventDefault()
+    // event.preventDefault()
     let addedReview = await axios
       .post(`http://localhost:3001/api/reviews/${id}`, formState)
       .then((response) => {
@@ -32,26 +32,26 @@ const ReviewPage = () => {
         return error
       })
     setReviews([...reviews, addedReview.data])
-    setFormState({ author: '', unit: '', patientLoad: '', review: '', rating: '', id: ''})
+    setFormState({ author: '', unit: '', patientLoad: '', review: '', rating: ''})
     }
 
     const handleUpdate = async (event) => {
-      event.preventDefault()
+      // event.preventDefault()
       let fixedReview = await axios.put(`http://localhost:3001/api/reviews/${event.target.id}`, formState)
       console.log(event)
       setReviews([reviews, fixedReview.data])
-      setFormState({ author: '', unit: '', patientLoad: '', review: '', rating: '', id: ''})
+      setFormState({ author: '', unit: '', patientLoad: '', review: '', rating: ''})
     }
 
     const handleDelete = async (event) => {
-      event.preventDefault()
+      // event.preventDefault()
       let deletedReview = await axios.delete(`http://localhost:3001/api/reviews/${event.target.id}`, formState)
       setReviews([reviews, deletedReview.data])
     }
 
 
   return (
-    <div className="ReviewPage">
+    <div className="ReviewPage" id="reviewCard">
       {reviews ? reviews.map((review) => (
       <div className="reviews" key={review.id}>
       <h3>Review by: {review.author}</h3>
@@ -60,25 +60,21 @@ const ReviewPage = () => {
         <h4>Comments: {review.review}</h4>
         <h4>Typical Patient Load: {review.patientLoad}</h4>
         <h4>Overall Rating:{review.rating}/5</h4>
-        <form>
+        <form className='update'>
         <h3>Update Review: </h3>
-        <label htmlFor="author">Your Name: </label>
-        <input id="author" value={formState.name} onChange={handleChange} />
-        <label htmlFor="unit">Unit:</label>
-        <input id="unit" value={formState.description} onChange={handleChange} />
-        <label htmlFor="patientLoad">Patient Load: </label>
-        <input id="patientLoad" value={formState.name} onChange={handleChange} />
         <label htmlFor="review">Review:</label>
         <input id="review" value={formState.description} onChange={handleChange} />
         <label htmlFor="rating">Rating (out of 5): </label>
-        <input id="rating" value={formState.name} onChange={handleChange} />
+        {/* <input id="rating" value={formState.name} onChange={handleChange} />
+        <label htmlFor="id">ID: </label> */}
+      {/* <input id="id" value={formState.id} onChange={handleChange} /> */}
         <button id={review.id} className='update' onClick={handleUpdate}>Update</button>
+        <button className='delete' id={review.id} onClick={handleDelete}>Delete</button>
       </form> 
-        <button id={review.id} onClick={handleDelete}>Delete Review</button>
     </div>
     )): "" }
-    <form onSubmit={handleSubmit}>
-      <h3>Add Your Own Review: </h3>
+    <form className='form' onSubmit={handleSubmit}>
+      <h3>Add a New Review: </h3>
       <label htmlFor="author">Your Name: </label>
       <input id="author" value={formState.name} onChange={handleChange} />
       <label htmlFor="unit">Unit:</label>
@@ -89,8 +85,6 @@ const ReviewPage = () => {
       <input id="review" value={formState.description} onChange={handleChange} />
       <label htmlFor="rating">Rating (out of 5): </label>
       <input id="rating" value={formState.name} onChange={handleChange} />
-      <label htmlFor="id">ID (Update Only): </label>
-      <input id="id" value={formState.id} onChange={handleChange} />
       <button className='submit' type="submit">Submit</button>
     </form>
     <button><Link className='link' to='/'>Back to Home</Link></button>
