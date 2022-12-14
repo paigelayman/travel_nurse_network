2a223ea274675a1c3938298239daa9601b2effe5
Paige Layman <paigelayman96@gmail.com>
2022-12-14 15:15:31 -0500

    heroku deployment

--- config/config.js
+++ config/config.js
@@ -2,17 +2,14 @@ require('dotenv').config()
 module.exports = {
   development: {
     database: 'travel_rn_development',
-    host: '127.0.0.1',
     dialect: 'postgres'
   },
   test: {
     database: 'travel_rn_test',
-    host: '127.0.0.1',
     dialect: 'postgres'
   },
   production: {
     database: 'travel_rn_production',
-    host: '127.0.0.1',
     dialect: 'postgres'
   }
 }
